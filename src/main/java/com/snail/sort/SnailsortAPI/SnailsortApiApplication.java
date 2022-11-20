27261ad728cb182c09a0615aeaa324f38ea0790f
Maxime CURON <maxime.curon@std.heh.be>
2022-11-20 04:13:18 +0100

    SnailsortAPI v0.0.0

---
+++ src/main/java/com/snail/sort/SnailsortAPI/SnailsortApiApplication.java
@@ -0,0 +1,13 @@
+package com.snail.sort.SnailsortAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailsortApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailsortApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snail/sort/SnailsortAPI/SnailsortApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snail.sort.SnailsortAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailsortApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
